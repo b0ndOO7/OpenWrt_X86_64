bd065e65253495dd126e752db6beab8341444820
b0ndOO7 <39324942+b0ndOO7@users.noreply.github.com>
2021-06-22 10:24:06 +0800

    Update diy-part1.sh
    add xray
    remove appfilter,small

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,8 +19,11 @@
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/V2RaySSR/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/V2RaySSR/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/V2RaySSR/small' feeds.conf.default
 sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
 sed -i '$a src-git dockerman https://github.com/lisaac/luci-app-dockerman.git' feeds.conf.default
 sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash.git' feeds.conf.default
-sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git' feeds.conf.default
+#sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git' feeds.conf.default
+
+sed -i '$a src-git src-git Xray https://github.com/yichya/openwrt-xray.git' feeds.conf.default
+sed -i '$a src-git src-git AppXray https://github.com/yichya/luci-app-xray.git' feeds.conf.default
